1302f246c2c27aa4fc11da4a350bcaf6014fd100
MAlvar000 <alvaro.mateocarrero@usp.ceu.es>
2025-04-23 08:52:57 +0200

    Practica4

--- src/test/aplicacion/PrincipalTest.java
+++ src/test/aplicacion/PrincipalTest.java
@@ -4,10 +4,5 @@ import org.junit.jupiter.api.Test;
 
 public class PrincipalTest {
  
-    @Test
-    public void mainTest(){
-
-        String[] args = new String[]{"raton","rato"};
-        Principal.main(args);
-    }
+    
 }
